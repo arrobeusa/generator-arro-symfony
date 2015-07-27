c4186d09d16e5fa474fca3049a844d3bb3ecbe2e
Rob Rotarius <robert.rotarius@linkshare.com>
2015-07-27 16:28:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+Symfony2
+=====================
+
+generator-arro-symfony is a [Yeoman Generator](http://yeoman.io/generators/) to scaffold Symfony2 projects with sensible defaults, common bundles and frontend tools.
+
+It will create a new Symfony project, remove Assetic and replace it with either Gulp or Grunt.
+
+## Dependencies
+
+Mandatory dependencies :
+
+- [composer](https://getcomposer.org/download/)
+- [npm](http://nodejs.org/)
+- [yo](http://yeoman.io/)
+- [gulp](https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md#getting-started)
+- [grunt](http://gruntjs.com/getting-started)
+- [bower](http://bower.io/#install-bower)
+
+Optional dependencies :
+
+- [gem](https://www.ruby-lang.org/en/downloads/)
+- [compass](http://compass-style.org/install/)
+
+
+## What you can choose
+
+* Symfony 2 Standard Edition:
+ * [2.3](https://github.com/symfony/symfony-standard/tree/2.3)
+ * [2.5](https://github.com/symfony/symfony-standard/tree/2.5)
+ * [2.6](https://github.com/symfony/symfony-standard/tree/2.6)
+* Grunt:
+ * [grunt-compass](https://github.com/gruntjs/grunt-contrib-compass)
+ * [grunt-coffee](https://github.com/gruntjs/grunt-contrib-coffee)
+ * [grunt-sass](https://github.com/gruntjs/grunt-contrib-sass)
+ * [grunt-typescript](https://github.com/k-maru/grunt-typescript)
+ * [grunt-cssmin](https://github.com/gruntjs/grunt-contrib-cssmin)
+ * [grunt-uglify](https://github.com/gruntjs/grunt-contrib-uglify)
+ * [grunt-copy](https://github.com/gruntjs/grunt-contrib-copy)
+* Gulp:
+ * [gulp-ruby-sass](https://github.com/sindresorhus/gulp-ruby-sass)
+ * [gulp-copy](https://github.com/klaascuvelier/gulp-copy)
+ * [gulp-concat](https://github.com/wearefractal/gulp-concat)
+* Symfony2 Bundle:
+ * [DoctrineFixturesBundle](https://github.com/doctrine/DoctrineFixturesBundle)
+ * [DoctrineMigrationsBundle](https://github.com/doctrine/DoctrineMigrationsBundle)
+ * [DoctrineMongoDBBundle](https://github.com/doctrine/DoctrineMongoDBBundle)
+* Bootstrap 3.3 (SASS)
+
+## Default workflow
+
+* Installs Symfony
+* Removes Assetic
+ * Only if using grunt: **(load-grunt-tasks)**
+ * Only if using grunt-typescript or grunt-coffeee: **(grunt-uglify)**
+ * Only if using grunt-sass or grunt-compass: **(grunt-css-min)**
+ * Only if using gulp **(gulp-load-plugins)**
+ * Only if using gulp-ruby-sass **(gulp-minify-css, gulp-plumber, gulp-rename, gulp-watch)**
+ * Only if using gulp-concat **(gulp-uglify)**
+* Starts the automatic execution of `bower`, `npm` and `composer` after scaffolding has finished.
+
+## Assets location
+
+Assets are stored in the **app/Resources/** folder :
+
+* app/Resources/scss
+* app/Resources/fonts
+* app/Resources/...
+
+Bower dependencies (like Bootstrap 3.3) are installed in the **app/Resources/libs/** folder, thanks to a custom [.bowerrc](http://bower.io/docs/config/#bowerrc-specification) file.
+
+
+## Getting Started
+
+- Install: `npm install -g yo`
+- Install: `npm install -g generator-arro-symfony`
+- Run: `yo arro-symfony --force`
+
+## Contribute
+
+`generator-arro-symfony` is fork-friendly : you can maintain a custom version, in which you `npm install && npm link` so that you can still use it with `yo arro-symfony`, or with the name of your choice.
---
+++ generators/app/index.js
@@ -0,0 +1,376 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var path = require('path');
+var yaml = require('js-yaml');
+var fs = require('fs');
+
+module.exports = yeoman.generators.Base.extend({
+    initializing: function () {
+//        this.pkg = require('../package.json');
+//        var Jolicode = '\n        ' + chalk.yellow('##') + '       ##            ##  ##   #######                   ##                ' + chalk.yellow('##') + '  ' + chalk.yellow('##') + '  \n      ' + chalk.yellow('##') + '         ##            ##      ##     ##                  ##               ' + chalk.yellow('##') + '    ' + chalk.yellow('##') + '  \n     ' + chalk.yellow('##') + '          ##   ######   ##  ##  ##          ######    #######   ######     ' + chalk.yellow('##') + '      ' + chalk.yellow('##') + '  \n     ' + chalk.yellow('##') + '          ##  ##    ##  ##  ##  ##         ##    ##  ##    ##  ########   ' + chalk.yellow('##') + '       ' + chalk.yellow('##') + '  \n      ' + chalk.yellow('##') + '  ##    ###  ##    ##  ##  ##  ##     ##  ##    ##  ##    ##  ##        ' + chalk.yellow('##') + '       ' + chalk.yellow('##') + '  \n       ' + chalk.yellow('##') + '   #####     ######   ##  ##   #######    ######    #######   ######  ' + chalk.yellow('##') + '       ' + chalk.yellow('##');
+//        var JolicodeDesc = '\n\n   A Yeoman generator for the Symfony2 framework\n\n   Created by ' + chalk.yellow('@JoliCode ') + ' & ' + chalk.blue('@lbrunet_com') + '\n   ' + chalk.cyan('http://jolicode.com/') + '\n';
+//        this.log(Jolicode);
+//        this.log(JolicodeDesc);
+    },
+
+    askSymfonyStandard: function () {
+        var done = this.async();
+
+        this.SymfonyStandardDistribution = {
+            username: 'symfony',
+            repository: 'symfony-standard',
+            commit: '2.6'
+        };
+
+        var prompts = [{
+            type: 'confirm',
+            name: 'symfonyStandard',
+            message: 'Would you like to use the Symfony "Standard Edition" distribution ' + this.SymfonyStandardDistribution.commit,
+            default: true
+        }];
+
+        this.prompt(prompts, function(answers) {
+
+            if (answers.symfonyStandard) {
+                this.symfonyDistribution = this.SymfonyStandardDistribution;
+            } else {
+                this.symfonyDistribution = null;
+            }
+
+            done();
+        }.bind(this));
+    },
+
+    askSymfonyCustom: function () {
+        if (this.symfonyDistribution === null) {
+            var done = this.async();
+
+            console.log('Please provide GitHub details of the Symfony distribution you would like to use.');
+            console.log('e.g. http://github.com/symfony/symfony-standard/tree/[commit].');
+
+            var prompts = [{
+                type: 'list',
+                name: 'symfonyCommit',
+                message: 'Commit (commit/branch/tag)',
+                default: '2.6',
+                choices: ['2.3', '2.5', '2.6']
+            }];
+
+            this.prompt(prompts, function(answers) {
+
+                var repo = 'https://github.com/' + 'symfony' + '/' + 'symfony-standard' + '/tree/' + answers.symfonyCommit;
+                console.log('Thanks! I\'ll use ' + repo);
+                console.log('');
+
+                this.symfonyDistribution = {
+                    username: 'symfony',
+                    repository: 'symfony-standard',
+                    commit: answers.symfonyCommit
+                };
+
+                done();
+            }.bind(this));
+        }
+    },
+
+    askToolsExtension: function() {
+        var done = this.async();
+        var prompts = [{
+            type: 'list',
+            name: 'toolsExtension',
+            message: 'Which tools would you like to use?',
+            default: 'gulp',
+            choices: ['grunt', 'gulp']
+        }];
+        this.prompt(prompts, function(answers) {
+            this.toolsExtension = answers.toolsExtension;
+            done();
+        }.bind(this));
+    },
+
+    askGruntCustom: function () {
+        if (this.toolsExtension === 'grunt') {
+            var done = this.async();
+
+            var prompts = [{
+                type: 'checkbox',
+                name: 'gruntCustom',
+                message: 'Customize Gruntfile',
+                choices: [
+                    {
+                        name: 'grunt-compass',
+                        value: 'gruntCompass',
+                        checked: true
+                    },
+                    {
+                        name: 'grunt-coffee',
+                        value: 'gruntCoffee',
+                        checked: false
+                    },
+                    {
+                        name: 'grunt-sass',
+                        value: 'gruntSass',
+                        checked: false
+                    },
+                    {
+                        name: 'grunt-typescript',
+                        value: 'gruntTypescript',
+                        checked: true
+                    },
+                    {
+                        name: 'grunt-copy',
+                        value: 'gruntCopy',
+                        checked: true
+                    }
+                ]
+            }];
+
+            this.prompt(prompts, function(answers) {
+                function hasFeature(feat) {
+                    return answers.gruntCustom.indexOf(feat) !== -1;
+                }
+
+                this.gruntcompass = hasFeature('gruntCompass');
+                this.gruntcoffee = hasFeature('gruntCoffee');
+                this.gruntSass = hasFeature('gruntSass');
+                this.gruntTypescript = hasFeature('gruntTypescript');
+                this.gruntCopy = hasFeature('gruntCopy');
+
+                done();
+            }.bind(this));
+        }
+    },
+
+    askGulpCustom: function() {
+        if (this.toolsExtension === 'gulp') {
+            var done = this.async();
+
+            var prompts = [{
+                type: 'checkbox',
+                name: 'gulpCustom',
+                message: 'Customize Gulpfile',
+                choices: [
+                    {
+                        name: 'gulp-ruby-sass',
+                        value: 'gulpRubySass',
+                        checked: true
+                    },
+                    {
+                        name: 'gulp-copy',
+                        value: 'gulpCopy',
+                        checked: false
+                    },
+                    {
+                        name: 'gulp-concat',
+                        value: 'gulpConcat',
+                        checked: false
+                    }
+                ]
+            }];
+
+            this.prompt(prompts, function (answers) {
+                function hasFeature(feat) {
+                    return answers.gulpCustom.indexOf(feat) !== -1;
+                }
+
+                this.gulpRubySass = hasFeature('gulpRubySass');
+                this.gulpCopy = hasFeature('gulpCopy');
+                this.gulpConcat = hasFeature('gulpConcat');
+
+                done();
+            }.bind(this));
+        }
+    },
+
+    askBowerStandard: function() {
+        var done = this.async();
+
+        var prompts = [{
+            type: 'confirm',
+            name: 'bowerStandard',
+            message: 'Would you like to use "BootStrap 3.3"?',
+            default: true
+        }];
+
+        this.prompt(prompts, function (answers) {
+            this.bowerStandard = answers.bowerStandard;
+            done();
+        }.bind(this));
+    },
+
+    symfonyBase: function() {
+        var done = this.async();
+        var appPath = this.destinationRoot();
+
+        this.remote(
+            this.symfonyDistribution.username,
+            this.symfonyDistribution.repository,
+            this.symfonyDistribution.commit,
+            function (err, remote) {
+                if (err) {
+                    return done(err);
+                }
+                remote.directory('.', path.join(appPath, '.'));
+                done();
+            }
+        );
+    },
+
+    askbundle: function() {
+        var done = this.async();
+
+        var prompts = [{
+            type: 'checkbox',
+            name: 'addBundle',
+            message: 'Which bundle would you like to use?',
+            choices: [
+                {
+                    name: 'DoctrineFixturesBundle',
+                    value: 'fixturebundle',
+                    checked: true
+                },
+                {
+                    name: 'DoctrineMigrationsBundle',
+                    value: 'migrationbundle',
+                    checked: false
+                },
+                {
+                    name: 'DoctrineMongoDBBundle',
+                    value: 'mongoDBbundle',
+                    checked: false
+                }
+            ]
+
+        }];
+
+        this.prompt(prompts, function(answers){
+            function hasFeature(feat){
+                return answers.addBundle.indexOf(feat) !== -1;
+            }
+
+            this.fixturebundle = hasFeature('fixturebundle');
+            this.migrationbundle = hasFeature('migrationbundle');
+            this.mongoDBbundle = hasFeature('mongoDBbundle');
+            done();
+        }.bind(this));
+
+    },
+
+    writing: {
+        app: function () {
+            if (this.toolsExtension === 'grunt') {
+                this.template('_Gruntfile.js', 'Gruntfile.js');
+            }
+            if (this.toolsExtension === 'gulp') {
+                this.template('_Gulpfile.js', 'Gulpfile.js');
+            }
+            this.fs.copy(
+                this.templatePath('_gitignore'),
+                this.destinationPath('.gitignore')
+            );
+            this.fs.copy(
+                this.templatePath('_bowerrc'),
+                this.destinationPath('.bowerrc')
+            );
+            this.template('_bower.json', 'bower.json');
+            this.template('_package.json', 'package.json');
+        },
+
+        projectfiles: function () {
+            this.fs.copy(
+                this.templatePath('editorconfig'),
+                this.destinationPath('.editorconfig')
+            );
+            this.fs.copy(
+                this.templatePath('jshintrc'),
+                this.destinationPath('.jshintrc')
+            );
+        },
+
+        buildParametersYml: function(){
+            this.template('_parameters.yml', 'app/config.parameters.yml');
+        },
+
+        buildComposerJson: function(){
+            this.template("_composer.json", "composer.json");
+            this.template("_composer.lock", "composer.lock");
+        },
+
+        buildConfig: function() {
+            this.template("_config.yml", "app/config/config.yml");
+        },
+
+        buildAppKernel: function() {
+            this.template("_AppKernel.php", "./app/AppKernel.php");
+        },
+
+    },
+
+    install: function () {
+        this.installDependencies({
+            skipInstall: this.options['skip-install']
+        });
+    },
+
+    end: {
+
+        cleanComposer: function () {
+            var done = this.async();
+
+            var composerContents = this.readFileAsString('composer.json');
+            var composerParse = JSON.parse(composerContents);
+            delete composerParse.require['symfony/assetic-bundle'];
+            var data = JSON.stringify(composerParse, null, 4);
+            fs.writeFileSync('composer.json', data);
+
+            done();
+        },
+
+        cleanConfig: function () {
+            var done = this.async();
+
+            var confDev = yaml.safeLoad(fs.readFileSync('app/config/config_dev.yml'));
+            delete confDev.assetic;
+            var newConfDev = yaml.dump(confDev, {indent: 4});
+            fs.writeFileSync('app/config/config_dev.yml', newConfDev);
+
+            var conf = yaml.safeLoad(fs.readFileSync('app/config/config.yml'));
+            delete conf.assetic;
+            var newConf = yaml.dump(conf, {indent: 4});
+            fs.writeFileSync('app/config/config.yml', newConf);
+
+            done();
+        },
+
+        updateAppKernel: function () {
+            console.log('This will add the custom bundles to Symfony\'s AppKernel');
+            var appKernelPath = 'app/AppKernel.php';
+            var appKernelContents = this.readFileAsString(appKernelPath);
+
+            var newAppKernelContents = appKernelContents.replace('new Symfony\\Bundle\\AsseticBundle\\AsseticBundle(),', '');
+            fs.writeFileSync(appKernelPath, newAppKernelContents);
+        },
+
+        addBundleComposer: function(){
+
+            if (this.fixturebundle) {
+                this.spawnCommand('composer', ['require', 'doctrine/doctrine-fixtures-bundle', '--no-update']);
+            }
+            if (this.migrationbundle) {
+                this.spawnCommand('composer', ['require', 'doctrine/migrations', '@dev',  '--no-update']);
+                this.spawnCommand('composer', ['require', 'doctrine/doctrine-migrations-bundle', '@dev',  '--no-update']);
+            }
+            if (this.mongoDBbundle) {
+                this.spawnCommand('composer', ['require', 'doctrine/mongodb-odm', '@dev', '--no-update']);
+                this.spawnCommand('composer', ['require', 'doctrine/mongodb-odm-bundle', '@dev', '--no-update']);
+            }
+
+            this.spawnCommand('composer', ['install', '--no-interaction']);
+        }
+
+
+
+    }
+});
